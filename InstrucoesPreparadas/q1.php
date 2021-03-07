88459a76c726296385aefd98f57c5097506ba786
vivianne.chaves17@gmail.com <vivianne.chaves17@gmail.com>
2021-03-07 11:42:03 -0300

    Trabalho utilizando instruções preparadas

---
+++ InstrucoesPreparadas/q1.php
@@ -0,0 +1,21 @@
+<html>
+  <head>
+    <title>Página inicial</title>
+    <meta charset="utf-8"/> 
+  </head>
+    
+  <body> 
+    <h1 style="text-align:center; background-color: orange; ">Atividade - Tabela de Números Randômicos<br><br>Por Vivianne Chaves de Miranda</h1>
+
+    <p style="font-size:18px; color:red;"><b>O objetivo dessa atividade é gerar bilhetes com números aleatórios para um concurso.<br>
+    Portanto, ao apertar o botão Gerar, você irá gerar um bilhete com: o número do seu bilhete, a data de emissão e os 3 números que podem ser sorteados.<br>E, ao apertar o botão Consulta você pode conferir os bilhetes já existentes.</p>
+    
+
+    <button onclick="window.location.href ='inserir.php'">Gerar</button>
+    <button onclick="window.location.href ='consulta.php'">Consulta</button><br><br><br>
+
+    <label>Link para baixar os aquivos:</label>
+    <a href="/InstrucoesPreparadas/InstrucoesPreparadas.zip" download="Atividade_instruções_preparadas">Download - Atividade com instruções preparadas</a>
+</body>
+
+</html>
